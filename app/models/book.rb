c18430dc1df38d7258e35126e627498b196c3f4d
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-05 16:44:50 +0200

    Added paperclip

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
 
   belongs_to :user
-
+  has_attached_file :image
+  has_attached_file :resource
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104192414) do
+ActiveRecord::Schema.define(version: 20170105144105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,18 @@ ActiveRecord::Schema.define(version: 20170104192414) do
     t.string   "author"
     t.text     "description"
     t.integer  "price"
-    t.boolean  "availability", default: true
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.boolean  "availability",          default: true
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "resource_file_name"
+    t.string   "resource_content_type"
+    t.integer  "resource_file_size"
+    t.datetime "resource_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
