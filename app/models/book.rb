fefcd7a6af13f10607898d1453084b19162f2dce
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-05 17:52:16 +0200

    AWS is now configured with paperclip

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,11 @@ class Book < ApplicationRecord
   belongs_to :user
   has_attached_file :image
   has_attached_file :resource
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/, massage: 'Only image allowed'
+  validates_attachment :resource, content_type: { content_type: 'application/pdf' }, massage: 'Only pdf allowed'
+
+  validates :image, attachment_presence: true
+  validates :resource, attachment_presence: true
+
 end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,17 @@ module Estuk
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_region => ENV['AWS_REGION'],
+        :s3_host_name => "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+        :s3_credentials => {
+            :bucket => ENV['AWS_BUCKET'],
+            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        }
+    }
+
   end
 end
