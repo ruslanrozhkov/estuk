884369e04b52e3f2fdb2aa1f6064ed0a30166601
Ruslan Rozhkov <bladteam@gmail.com>
2017-01-06 21:47:27 +0200

    Added state to sales

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105190146) do
+ActiveRecord::Schema.define(version: 20170106182001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,11 @@ ActiveRecord::Schema.define(version: 20170105190146) do
     t.integer  "book_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "state"
+    t.string   "stripe_id"
+    t.string   "stripe_token"
+    t.text     "error"
+    t.integer  "amount"
   end
 
   create_table "users", force: :cascade do |t|
